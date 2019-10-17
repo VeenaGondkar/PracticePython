2de0800d7a7dc5ba8a8d4e03719f27f472dde20f
Veena Gondkar <veena@factom.org>
2019-10-17 17:17:26 -0500

    added webscraping example

--- keepslive.py
+++ keepslive.py
@@ -1,6 +1,15 @@
-import time
 
+from urllib.request import urlopen
+import re
 
-while True:
-    print("hello")
-    time.sleep(1)
+#connect to a URL
+website = urlopen("https://www.google.com/")
+
+#read html code
+html = website.read()
+
+# use re.findall to get all the links
+links = re.findall('"((http|ftp)s?://.*?)"', str(html))
+
+for i in links:
+    print(i[0])
