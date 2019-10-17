923a97d60de9f2470a45f4ce548e86d358e853f7
Veena Gondkar <veena@factom.org>
2019-10-17 16:08:41 -0500

    updated the EOM count script

--- Parse_EOM_timestamp.py
+++ Parse_EOM_timestamp.py
@@ -1,6 +1,8 @@
 import subprocess
 
-bashcommand = 'grep -E "EOM-" /home/factom/factomd/fnode0_networkinputs.txt | grep -v Drop | grep -v Embed |  grep -Eo "[0-9]{2}:[0-9]{2}:[0-9]{2} +$" > time.txt'
+# bashcommand = 'grep -E "EOM-" /home/factom/factomd/fnode05_networkinputs.txt | grep -v Drop | grep -v Embed |  grep -Eo "[0-9]{2}:[0-9]{2}:[0-9]{2} +$" > time.txt'
+bashcommand = 'grep -E "EOM-" /home/factom/go/src/github.com/FactomProject/factomd/simTest/fnode02_networkinputs.txt | grep -v Drop | grep -v Embed |  grep -Eo "[0-9]{2}:[0-9]{2}:[0-9]{2} +$" > time.txt'
+
 output = subprocess.check_output(bashcommand, shell=True)
 
 
@@ -11,9 +13,9 @@ content = [x.strip() for x in content]
 result = dict((i, content.count(i)) for i in content)
 counter = 0
 for i in result:
-    if result[i] == 1:
-        counter += 1
-        print(f"{i} : {result[i]}")
+    # if result[i] == 2:
+    counter += 1
+    print(f"{i} : {result[i]}")
 
 print(f"total number of times the EOM was seen {counter}")
 
